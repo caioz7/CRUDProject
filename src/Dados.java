aa79c2cde2fd914e64ac67ac42e2e52120ff4f5e
caioz7 <caio_z7_mga@hotmail.com>
2016-07-15 15:34:54 -0300

    Tentar novamente no windows

---
+++ src/Dados.java
@@ -0,0 +1,18 @@
+
+public class Dados {
+	
+	
+	public void CreateData(){
+		System.out.println("Esta opcao representa a insercao de dados");
+	}
+	public void ReadData(){
+		System.out.println("Esta opcao representa a leitura de dados");
+	}
+	public void UpdateData(){
+		System.out.println("Esta opcao representa a alteracao de dados e retorna os dados atualizados");
+	}
+	public void DeleteData(){
+		System.out.println("Esta opcao representa a remocao de dados");
+	}
+
+}
