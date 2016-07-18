bbdea9ebe1abca6394fd4b8dabed61907a5dbe9f
caioz7 <caio_z7_mga@hotmail.com>
2016-07-18 13:50:57 -0300

    Inserindo conexão com o Banco de Dados

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,10 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Dados pessoa = new Dados();
-		pessoa.CreateData();
+		Scanner scanf = new Scanner(System.in);
+		System.out.print("Insira o servidor: ");
+		String servidorBanco = scanf.next();
+		System.out.print("Insira o nome do Banco de Dados: ");
+		String nomeBanco = scanf.next();
+		System.out.print("Insira o usuario do Banco de Dados: ");
+		String usuarioBanco = scanf.next();
+		System.out.print("Insira a senha do usuario " + usuarioBanco + " : ");
+		String senhaBanco = scanf.next();
+		
+		String url = "jdbc:mysql://"+servidorBanco+"/"+nomeBanco;
+		try{
+			Connection conexao = DriverManager.getConnection(url,usuarioBanco,senhaBanco);
+			System.out.println("Conectado");
+			PreparedStatement pesquisa = conexao.prepareStatement("select * from Clientes");
+			ResultSet resultado = pesquisa.executeQuery();
+			while(resultado.next()){
+				String nome = resultado.getString("nome");
+				String idade = resultado.getString("idade");
+				System.out.println("Nome: " + nome + "\tIdade: " + idade);
+
+			}
+		}catch(Exception erro){
+			 System.err.println("Falha na conexão!");
+		}
 	}
 
 }
